af8e46a5c855c7bafe69473b93bd75a22de5c888
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2024-01-24 09:40:18 +0100

    fix: dynamic import user model

--- commands/create_user.ts
+++ commands/create_user.ts
@@ -1,6 +1,5 @@
 import { BaseCommand } from '@adonisjs/core/ace'
 import type { CommandOptions } from '@adonisjs/core/types/ace'
-import User from "#models/user";
 
 export default class CreateUser extends BaseCommand {
   static commandName = 'create:user'
@@ -12,6 +11,7 @@ export default class CreateUser extends BaseCommand {
 
   async run() {
     this.logger.info('Hello world from "CreateUser"')
+    const { default: User } = await import('#models/user');
     await User.create({fullName: 'test', email: 'test@example.com', password: 'verysecurepassword'});
   }
 }

9816755c058ec2963ab0522ece0063a080d9948f
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2024-01-24 11:28:51 +0100

    fix: update auth module and import static User

--- commands/create_user.ts
+++ commands/create_user.ts
@@ -1,5 +1,6 @@
 import { BaseCommand } from '@adonisjs/core/ace'
 import type { CommandOptions } from '@adonisjs/core/types/ace'
+import User from "#models/user";
 
 export default class CreateUser extends BaseCommand {
   static commandName = 'create:user'
@@ -11,7 +12,6 @@ export default class CreateUser extends BaseCommand {
 
   async run() {
     this.logger.info('Hello world from "CreateUser"')
-    const { default: User } = await import('#models/user');
     await User.create({fullName: 'test', email: 'test@example.com', password: 'verysecurepassword'});
   }
 }
